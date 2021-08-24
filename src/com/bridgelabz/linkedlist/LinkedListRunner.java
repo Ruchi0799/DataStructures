241f8e52434c70caf6e2d907c5c4bfcef8e79608
Ruchi Mandve <ruchi.mandve@morningstar.com>
2021-08-24 20:09:43 +0530

    [ruchi]: Added 56,30,70 in linkedlist in order 56-30-70

--- src/com/bridgelabz/linkedlist/LinkedListRunner.java
+++ src/com/bridgelabz/linkedlist/LinkedListRunner.java
@@ -2,13 +2,14 @@ package com.bridgelabz.linkedlist;
 
 public class LinkedListRunner {
     public static void main(String[] args) {
-        MyNode<Integer> myFirstNode=new MyNode<>(70);
+        MyNode<Integer> myFirstNode=new MyNode<>(56);
         MyNode<Integer> mySecondNode=new MyNode<>(30);
-        MyNode<Integer> myThirdNode=new MyNode<>(56);
+        MyNode<Integer> myThirdNode=new MyNode<>(70);
         MyLinkedList myLinkedList=new MyLinkedList();
         myLinkedList.add(myFirstNode);
         myLinkedList.add(mySecondNode);
         myLinkedList.add(myThirdNode);
         myLinkedList.printMyNodes();
+
     }
 }
