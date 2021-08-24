86647b38f3e8b0e65baba5a3ef4d619d9121884d
Ruchi Mandve <ruchi.mandve@morningstar.com>
2021-08-25 01:31:15 +0530

    [ruchi]: implemented ADD nodes in queue using linkedlist

---
+++ src/com/brideglabz/stackandqueue/MyQueue.java
@@ -0,0 +1,50 @@
+package com.brideglabz.stackandqueue;
+
+public class MyQueue<K> {
+    public INode<K> head;
+    public INode<K> tail;
+
+    public MyQueue() {
+
+    }
+
+    public void enqueue(INode<K> newNode)
+    {
+        add(newNode);
+    }
+
+    public void add(INode<K> newNode) {
+        if (this.head == null) {
+            this.head = newNode;
+        }
+        if (this.tail == null) {
+            this.tail = newNode;
+        }
+        else {
+            INode tempNode = this.head;
+            while (tempNode.getNext()!=null)
+            {
+                tempNode=tempNode.getNext();
+            }
+            tempNode.setNext(newNode);
+            newNode.setNext(null);
+        }
+    }
+
+
+
+    public void printMyNodes() {
+
+        StringBuffer myNodes = new StringBuffer("My Nodes: ");
+        INode temp = this.head;
+        while (temp.getNext()!=null) {
+            myNodes.append(temp.getKey());
+            myNodes.append("->");
+            temp = temp.getNext();
+        }
+        myNodes.append(temp.getKey());
+        System.out.print(myNodes);
+
+
+    }
+}
